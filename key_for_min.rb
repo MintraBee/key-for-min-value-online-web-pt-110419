cf84ce6a9ab2272478d7bc8928a1bcbd90b71088
Mintra Phophaijit <bluembc@gmail.com>
2019-12-08 20:44:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-hash = {:blake => 500, :ashley => 2, :adam => 1}
-hash
+name_hash = {:blake => 500, :ashley => 2, :adam => 1}
+name_hash.each 
+do |name_hash
 end
 
