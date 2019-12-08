052de13344fc794d0d1b7ea361dddd26705103e0
Mintra Phophaijit <bluembc@gmail.com>
2019-12-08 20:46:16 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,26 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+name_hash = {:blake => 500, :ashley => 2, :adam => 1}
+name_hash.each 
+do |name_hash, age|
+end
 
+
+def key_for_min_value(name_hash)
+name_hash = {:blake => 500, :ashley => 2, :adam => 1}
+name_hash.each 
+do |name_hash, age|
+end
+
+
+def key_for_min_value(name_hash)
+name_hash = {:blake => 500, :ashley => 2, :adam => 1}
+key_for_min_value(name_hash)
+end
+
+
+def key_for_min_value(name_hash)
+name_hash = {:blake => 500, :ashley => 2, :adam => 1}
+name_hash.sort
 end
